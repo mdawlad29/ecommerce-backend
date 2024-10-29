161a167b10e2cbb223f0acae51abd6e429eda288
mdawlad29 <mdawladbdinfo@gmail.com>
2024-10-30 00:50:16 +0600

    Code formet fixed

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
-import express,{Express,Request,Response} from "express";
+import express, { Express, Request, Response } from "express";
 import { PORT } from "./secret";
 import rootRoutes from "./routes";
 import { PrismaClient } from "@prisma/client";
 import { errorMiddleware } from "./middleware/errors";
-import { SignupSchema } from "./schema/users";
 
-const app:Express = express();
-app.use(express.json())
+const app: Express = express();
+app.use(express.json());
 
-app.use("/api",rootRoutes)
+app.get("/", (req: Request, res: Response) => {
+  res
+    .status(200)
+    .json({ status: 200, message: "Hello from the application.", data: [] });
+});
 
-export const prismaClient=new PrismaClient({
-    log:["query"]
-})
+app.use("/api", rootRoutes);
 
-app.use(errorMiddleware)
+export const prismaClient = new PrismaClient({
+  log: ["query"],
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+app.use(errorMiddleware);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
